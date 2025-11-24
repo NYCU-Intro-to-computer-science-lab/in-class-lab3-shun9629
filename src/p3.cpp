60bf2eb15b7fc4206141e3c5d8abb9002dd9831a
quan0715 <quan787887@gmail.com>
2025-11-25 05:42:05 +0800

    revert

--- src/p3.cpp
+++ src/p3.cpp
@@ -2,21 +2,9 @@
 
 #include <iostream>
 using namespace std;
-bool isPrime(int n){
-    for(int i=2;i<n;i++){
-        if(n % i == 0){
-            return false;
-        }
-    }
-    return true;
-}
+
 int main() {
-    int n; cin >> n;
-    for(int i=2; i<=n/2;i++){
-        if(isPrime(i) && isPrime(n-i)){
-            cout << i << " " << n-i << endl;
-            break;
-        }
-    }
+    
+    
     return 0;
 }
